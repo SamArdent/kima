3371b5f0a929347c033a4e8057950e4fa95fae5a
Daeron <darren@joycat.co.uk>
2022-03-30 06:05:08 +0100

    make it better

---
+++ config.py
@@ -0,0 +1,2 @@
+token = "OTU4MDM1NDI0NzYzMzE0MjI3.YkHeBA.HqMlA7gI7HaayXkLsCLddmOdaog"
+clientid = 958035424763314227
---
+++ main.py
@@ -0,0 +1,13 @@
+import discord
+from config import token
+
+class Kima(discord.Client):
+    async def on_ready(self):
+        print(f'Logged on as {self.user}!')
+
+    async def on_message(self, message):
+        print(f'Message from {message.author}: {message.content}')
+
+
+kima = Kima()
+kima.run(token)
